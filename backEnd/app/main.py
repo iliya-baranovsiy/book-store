251e43cdd67caa91c212f1849db2e438d988864a
iliya-baranovsiy <mr.Fredi2017@mail.ru>
2026-07-08 19:55:49 +0300

    [ADD]: add sending picture

--- backEnd/app/main.py
+++ backEnd/app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from .api import router
 
 app = FastAPI(
     title="Books API",
     version="1.0.0"
 )
+app.mount(
+    "/uploads",
+    StaticFiles(directory="app/uploads"),
+    name="uploads"
+)
+
 app.include_router(router)
